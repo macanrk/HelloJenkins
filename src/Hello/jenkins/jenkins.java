6ecb62b48c2e5a596968fc27f23ba82d43aa5b92
macanrk <macanrk@yahoo.com>
2014-09-24 15:50:38 -0700

    first commit
    1st commot

---
+++ src/Hello/jenkins/jenkins.java
@@ -0,0 +1,14 @@
+package Hello.jenkins;
+
+public class jenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to my world");
+		System.out.println("Welcome");
+		//added extra message
+
+	}
+
+}
