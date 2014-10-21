5fda6aa8992d3bca477ba206cef27fc4ca571a18
macanrk <macanrk@users.noreply.github.com>
2014-10-21 14:38:41 -0500

    checking  gerrithub
    trying to play with gerrithub

--- src/Hello/jenkins/jenkins.java
+++ src/Hello/jenkins/jenkins.java
@@ -6,7 +6,7 @@ public class jenkins {
 		// TODO Auto-generated method stub
 
 		System.out.println("Welcome to my world");
-		System.out.println("Welcome");
+		//System.out.println("Welcome");
 		//commented extra message
 
 	}
